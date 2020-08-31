00194bd32af320dd84fc306d7a604eaf613b1b45
codethro <64641596+codethro@users.noreply.github.com>
2020-08-31 19:15:42 +0300

    diablo
    aslan

---
+++ commands/text/diablo.js
@@ -0,0 +1,21 @@
+const ytdl = require('ytdl-core');
+module.exports = {
+	name: 'diablo',
+	description: '🦁',
+	category: "info",
+	cooldown: 5,
+	async execute(message, client) {		
+		const voiceChannel = message.member.voice.channel;
+
+		if (!voiceChannel) {
+			return message.reply('please join a voice channel first!');
+		}
+
+		voiceChannel.join().then(connection => {
+			const stream = ytdl('https://www.youtube.com/watch?v=KS1oIN6Fq4w', { filter: 'audioonly' });
+			const dispatcher = connection.play(stream);
+
+			dispatcher.on('finish', () => voiceChannel.leave());
+		});
+	}
+};
