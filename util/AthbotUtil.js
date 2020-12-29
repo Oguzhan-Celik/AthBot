6be7967e4727f8297658ac911f6d6a946a252c9a
codethro <64641596+codethro@users.noreply.github.com>
2020-12-29 13:15:49 +0300

    Update AthbotUtil.js

--- util/AthbotUtil.js
+++ util/AthbotUtil.js
@@ -1,13 +1,26 @@
 module.exports = {
-    canModifyQueue(member) {
-      const { channel } = member.voice;
-      const botChannel = member.guild.me.voice.channel;
-  
-      if (channel !== botChannel) {
-        member.send("You need to join the voice channel first!").catch(console.error);
-        return false;
-      }
-  
-      return true;
+  canModifyQueue(member) {
+    const { channel } = member.voice;
+    const botChannel = member.guild.me.voice.channel;
+
+    if (channel !== botChannel) {
+      member
+        .send("You need to join the voice channel first!")
+        .catch(console.error);
+      return false;
     }
-  };
+
+    return true;
+  },
+
+  removeDuplicates(arr) {
+    return [...new Set(arr)];
+  },
+
+  capitalise(string) {
+    return string
+      .split(" ")
+      .map((str) => str.slice(0, 1).toUpperCase() + str.slice(1))
+      .join(" ");
+  },
+};
