a866fdd3ee5c3994d4215bb59348a4707a6d1803
codethro <64641596+codethro@users.noreply.github.com>
2020-12-29 16:27:37 +0300

    Update help.js

--- commands/text/help.js
+++ commands/text/help.js
@@ -19,7 +19,7 @@ module.exports = {
       )
       .setColor("ORANGE");
 
-    if (args[0] === cmd) {
+    if (args[0] === cmd.name || cmd.aliases) {
       helpEmbed.setAuthor(
         `${this.client.utils.capitalise(cmd.name)} Command Help`,
         "https://i.imgur.com/HtCrD3H.jpg?1"
