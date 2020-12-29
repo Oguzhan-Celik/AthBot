f81ba11eb2656b705be5f0d329706095e70cd593
codethro <64641596+codethro@users.noreply.github.com>
2020-12-29 14:40:11 +0300

    Update help.js

--- commands/text/help.js
+++ commands/text/help.js
@@ -11,30 +11,29 @@ module.exports = {
       .setTitle("AthBot Help Menu")
       .setDescription("These are the available commands for AthBot")
       .setColor("RANDOM")
-      .addField("**Music**", [
-        commands
-          .filter((cmd) => cmd.category == "Music")
-          .forEach((cmd) => {
-            
-              `\`${message.client.prefix}${cmd.name} ${
-                cmd.aliases ? `(${cmd.aliases})` : ""
-              }\``,
-              
-            
-          }),
-      ])
+      .addField("**Music**");
+    commands
+      .filter((cmd) => cmd.category == "Music")
+      .forEach((cmd) => {
+        helpEmbed.addField(
+          `\`${message.client.prefix}${cmd.name} ${
+            cmd.aliases ? `(${cmd.aliases})` : ""
+          }\``,
+          true
+        );
+      })
 
-      .addField("**Text**", [
-        commands
-          .filter((cmd) => cmd.category == "Text")
-          .forEach((cmd) => {
-            
-              `\`${message.client.prefix}${cmd.name} ${
-                cmd.aliases ? `(${cmd.aliases})` : ""
-              }\``,
-              
-          }),
-      ]);
+      .addField("**Text**");
+    commands
+      .filter((cmd) => cmd.category == "Text")
+      .forEach((cmd) => {
+        helpEmbed.addField(
+          `\`${message.client.prefix}${cmd.name} ${
+            cmd.aliases ? `(${cmd.aliases})` : ""
+          }\``,
+          true
+        );
+      });
 
     helpEmbed.setTimestamp();
 
