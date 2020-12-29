48d3106feddc2964f7fb2a9173caa8b48e47b5c8
codethro <64641596+codethro@users.noreply.github.com>
2020-12-29 16:15:09 +0300

    Update help.js
    yüksek ihtimal gg

--- commands/text/help.js
+++ commands/text/help.js
@@ -12,36 +12,54 @@ module.exports = {
         `${message.guild.name} Help Menu`,
         message.guild.iconURL({ dynamic: true })
       )
-      .setThumbnail("https://imgur.com/HtCrD3H")
+      .setThumbnail("https://i.imgur.com/HtCrD3H.jpg?1")
       .setFooter(
         `Requested by ${message.author.username}`,
         message.author.displayAvatarURL({ dynamic: true })
       )
-      .setColor("ORANGE")
-      .addField(
-        "**Music**",
-        commands
-          .filter((cmd) => cmd.category == "Music")
-          .map(
-            (cmd) =>
-              `\`${message.client.prefix}${cmd.name} ${
-                cmd.aliases ? `(${cmd.aliases})` : ""
-              }\``
-          )
-          .join(" ")
-      )
-      .addField(
-        "**Text**",
-        commands
-          .filter((cmd) => cmd.category == "Text")
-          .map(
-            (cmd) =>
-              `\`${message.client.prefix}${cmd.name} ${
-                cmd.aliases ? `(${cmd.aliases})` : ""
-              }\``
-          )
-          .join(" ")
+      .setColor("ORANGE");
+
+    if (args[0].toLowerCase() === cmd) {
+      helpEmbed.setAuthor(
+        `${this.client.utils.capitalise(cmd.name)} Command Help`,
+        "https://i.imgur.com/HtCrD3H.jpg?1"
       );
+      helpEmbed.setDescription([
+        `**❯ Aliases:** ${
+          cmd.aliases.length
+            ? cmd.aliases.map((alias) => `\`${alias}\``).join(" ")
+            : "No Aliases"
+        }`,
+        `**❯ Description:** ${cmd.description}`,
+        `**❯ Category:** ${cmd.category}`,
+      ]);
+    } else {
+      helpEmbed
+        .addField(
+          "**Music**",
+          commands
+            .filter((cmd) => cmd.category == "Music")
+            .map(
+              (cmd) =>
+                `\`${message.client.prefix}${cmd.name} ${
+                  cmd.aliases ? `(${cmd.aliases})` : ""
+                }\``
+            )
+            .join(" ")
+        )
+        .addField(
+          "**Text**",
+          commands
+            .filter((cmd) => cmd.category == "Text")
+            .map(
+              (cmd) =>
+                `\`${message.client.prefix}${cmd.name} ${
+                  cmd.aliases ? `(${cmd.aliases})` : ""
+                }\``
+            )
+            .join(" ")
+        );
+    }
 
     helpEmbed.setTimestamp();
 
