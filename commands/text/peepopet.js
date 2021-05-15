6e414cdca4f765a02bd64652172a713ec9aacb52
Codethro <oguz75364@gmail.com>
2021-05-15 13:42:15 +0300

    love

--- commands/text/peepopet.js
+++ commands/text/peepopet.js
@@ -1,48 +1,29 @@
-const Discord = require('discord.js')
-const Canvas = require('canvas')
+const { MessageEmbed } = require('discord.js')
 
 module.exports = {
-  name: 'peepopet',
-  description: 'pet pet pet',
+  name: 'love',
+  description: 'love meter',
   category: 'Text',
-  aliases: ['ppet'],
   cooldown: 5,
   async execute(message, args) {
-    if (!message.mentions.users.size > 0) {
-      var user = message.author
+    var user = message.client.users.cache.get(args < 1)
+
+    const love = Math.random() * 100
+    const loveIndex = Math.floor(love / 10)
+    const loveLevel = '❤'.repeat(loveIndex) + '💔'.repeat(10 - loveIndex)
+
+    const embed = new MessageEmbed()
+    embed.setColor('#FF283F')
+
+    if (message.mentions.users.size > 0) {
+      let user = message.mentions.users.first()
+      embed.setTitle(`${message.author.username} love ${user.username}!`)
     } else {
-      var user =
-        message.mentions.users.first() ||
-        message.client.users.cache.get(args < 1)
+      embed.setTitle(`${user} love`)
     }
 
-    const canvas = Canvas.createCanvas(300, 300)
-    const ctx = canvas.getContext('2d')
+    embed.setTitle(loveLevel)
 
-    const avatar = await Canvas.loadImage(
-      user.displayAvatarURL({ format: 'jpg' })
-    )
-    ctx.drawImage(avatar, 5, 160, 150, 150)
-    ctx.beginPath()
-    ctx.arc(80, 240, 75, 0, Math.PI * 2, true)
-    ctx.strokeStyle = '#000000'
-    ctx.lineWidth = 5
-    ctx.stroke()
-    ctx.closePath()
-    ctx.save()
-    ctx.clip()
-
-    ctx.restore()
-
-    var myGif = GIF()
-    myGif.load(
-      'https://cdn.betterttv.net/emote/5f21db8fcf6d2144653d8bd4/3x.gif'
-    )
-
-    ctx.drawImage(myGif.image, 0, 0)
-
-    const final = new Discord.MessageAttachment(canvas.toBuffer(), 'pet.gif')
-
-    return message.channel.send(final)
+    message.channel.send({ embed })
   },
 }
